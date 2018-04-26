9aa94a52bc4850213f66c7f7560a70d44ba299a6
lieoncx <lieoncx@gmail.com>
2018-04-26 11:23:26 +0800

    add stack description

--- Stack.playground/Contents.swift
+++ Stack.playground/Contents.swift
@@ -2,6 +2,13 @@
 
 import UIKit
 /// FILO
+/*
+ A stack is like an array but with limited functionality. You can only push to add a new element to the top of the stack, pop to remove the element from the top, and peek at the top element without popping it off.
+ 
+ Why would you want to do this? Well, in many algorithms you want to add objects to a temporary list at some point and then pull them off this list again at a later time. Often the order in which you add and remove these objects matters.
+ 
+ A stack gives you a LIFO or last-in first-out order. The element you pushed last is the first one to come off with the next pop. (A very similar data structure, the queue, is FIFO or first-in first-out.)
+ */
 public struct Stack<T> {
     fileprivate var array = [T]()
     public var isEmapty: Bool {
